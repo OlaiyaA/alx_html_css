a895f25df3c89e7bfc6deb046c1e4e976f926b7f
Abdulmalik Olaiya <olaiya.abdulmalik@gmail.com>
2025-10-15 00:07:08 +0100

    got the mobile nav link to appear perfectly

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,16 +1,26 @@
+
+    const mobileNav = document.getElementById("nav-links-mobile");
 // ===== MOBILE MENU TOGGLE =====
     const hamburger = document.getElementById("hamburger");
-    const navLinks = document.querySelector(".nav-links");
+    const navLinks = document.querySelector(".nav-linksa");
 
     hamburger.addEventListener("click", () => {
       hamburger.classList.toggle("active");
       navLinks.classList.toggle("open");
+      if (mobileNav.style.display === "flex") {
+    mobileNav.style.display = "none";
+  } else {
+    mobileNav.style.display = "flex";
+  }
     });
 
     // Optional: Close menu when a link is clicked
-    document.querySelectorAll(".nav-links a").forEach(link => {
+    document.querySelectorAll(".nav-linksa").forEach(link => {
       link.addEventListener("click", () => {
         hamburger.classList.remove("active");
         navLinks.classList.remove("open");
+        mobileNav.style.display = "none";
       });
     });
+
+
